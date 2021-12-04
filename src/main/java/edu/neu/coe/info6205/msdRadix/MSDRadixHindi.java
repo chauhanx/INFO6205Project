48c5f4659b86e6d571e8a20918c23c2eb2b29320
Aparna Shelar <shelar.a@northeastern.edu>
2021-12-03 23:19:09 -0500

    added randomword generator

--- src/main/java/edu/neu/coe/info6205/msdRadix/MSDRadixHindi.java
+++ src/main/java/edu/neu/coe/info6205/msdRadix/MSDRadixHindi.java
@@ -20,7 +20,7 @@ public class MSDRadixHindi {
             sort(a);
             Date end = new Date();
             System.out.println(end.getTime()-start.getTime());
-            //io.writeStream(a);
+            io.writeStream(a);
         }catch(FileNotFoundException ex){
             System.out.println(ex.getMessage());
         }
@@ -32,11 +32,10 @@ public class MSDRadixHindi {
     }
 
 
-    public static int char_at(String s, int d){
+    private static int char_at(String s, int d){
         if (d < s.length()){
-            if(s.charAt(d) == ' ') return 0;
-            int a = (int)(s.charAt(d));
-            //System.out.println(s + "  " + s.charAt(d) + "  " + a);
+            int a = (int)s.charAt(d);
+            //System.out.println(s.charAt(d)+" "+s+ " "+a);
             return a;
         }
         else return -1;
@@ -45,7 +44,7 @@ public class MSDRadixHindi {
 
     public static void sort(String[] arr){
         String[] aux = new String[arr.length];
-        sort(arr,aux, 0,arr.length-1,1);
+        sort(arr,aux, 0,arr.length-1,0);
     }
 
     public static void sort(String[] arr,String[] aux,int low,int high,int d){
@@ -53,11 +52,12 @@ public class MSDRadixHindi {
             return;
         }
         int[] count = new int[R+2];
+
         for(int i=low;i<=high;i++){
             int c =  char_at(arr[i],d);
-            if(c!=0)count[c+2]++;
+            count[c+2]++;
         }
-        for(int r=S;r<R;r++){
+        for(int r=0;r<R;r++){
             count[r+1] += count[r];
         }
 
@@ -68,7 +68,7 @@ public class MSDRadixHindi {
         for(int i=low;i<=high;i++){
             arr[i] = aux[i-low];
         }
-        for(int r=S;r<R;r++){
+        for(int r=0;r<R;r++){
             sort(arr,aux,low+count[r],low+count[r+1]-1,d+1);
         }
 
