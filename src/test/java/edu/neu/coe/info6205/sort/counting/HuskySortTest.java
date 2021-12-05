eca9c20713949e81e2df4cbc5a0169f67a48fdb1
Mohit <nagpalgmohit@gmail.com>
2021-12-05 05:57:56 -0500

    HuskySortTest

---
+++ src/test/java/edu/neu/coe/info6205/sort/counting/HuskySortTest.java
@@ -0,0 +1,101 @@
+package edu.neu.coe.info6205.sort.counting;
+
+import edu.neu.coe.info6205.huskySort.PureHuskySort;
+import edu.neu.coe.info6205.huskySortUtils.Coding;
+import edu.neu.coe.info6205.huskySortUtils.HuskyCoder;
+import edu.neu.coe.info6205.huskySortUtils.HuskyCoderFactory;
+import edu.neu.coe.info6205.sort.BaseHelper;
+import edu.neu.coe.info6205.util.PrivateMethodInvoker;
+
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class HuskySortTest {
+
+    private final BaseHelper<String> helper = new BaseHelper<>("dummy helper");
+
+    @Test
+    public void testSortString1() {
+        String[] xs = {"Hello", "Goodbye", "Ciao", "Willkommen"};
+        PureHuskySort<String> sorter = new PureHuskySort<>(HuskyCoderFactory.unicodeCoder, false, false);
+        sorter.sort(xs);
+        assertTrue("sorted", helper.sorted(xs));
+    }
+
+    @Test
+    public void testSortString2() {
+        PureHuskySort<String> sorter = new PureHuskySort<>(HuskyCoderFactory.asciiCoder, false, false);
+        final int N = 1000;
+        helper.init(N);
+        final String[] xs = helper.random(String.class, r -> r.nextLong() + "");
+        sorter.sort(xs);
+        assertTrue("sorted", helper.sorted(xs));
+    }
+
+    @Test
+    public void testSortString3() {
+        PureHuskySort<String> sorter = new PureHuskySort<>(HuskyCoderFactory.asciiCoder, false, false);
+        final int N = 1000;
+        helper.init(N);
+        final String[] xs = helper.random(String.class, r -> {
+            int x = r.nextInt(1000000000);
+            final BigInteger b = BigInteger.valueOf(x).multiply(BigInteger.valueOf(1000000));
+            return b.toString();
+        });
+        sorter.sort(xs);
+        assertTrue("sorted", helper.sorted(xs));
+    }
+
+    @Test
+    public void testSortString4() {
+        String[] xs = {"Hello", "Goodbye", "Ciao", "Willkommen"};
+        PureHuskySort<String> sorter = new PureHuskySort<>(HuskyCoderFactory.asciiCoder, false, false);
+        sorter.sort(xs);
+        assertTrue("sorted", helper.sorted(xs));
+    }
+
+    @Test
+    public void testSortString5() {
+        String[] xs = {"Hello", "Goodbye", "Ciao", "Welcome"};
+        PureHuskySort<String> sorter = new PureHuskySort<>(HuskyCoderFactory.asciiCoder, false, false);
+        sorter.sort(xs);
+        assertTrue("sorted", helper.sorted(xs));
+    }
+
+    @Test
+    public void testFloorLg() {
+        PrivateMethodInvoker privateMethodInvoker = new PrivateMethodInvoker(PureHuskySort.class);
+        assertEquals(Integer.valueOf(1), privateMethodInvoker.invokePrivate("floor_lg", 3));
+        assertEquals(Integer.valueOf(2), privateMethodInvoker.invokePrivate("floor_lg", 5));
+    }
+
+    @Test
+    public void testWithInsertionSort() {
+        PureHuskySort<String> sorter = new PureHuskySort<>(HuskyCoderFactory.asciiCoder, false, true);
+        PrivateMethodInvoker privateMethodInvoker = new PrivateMethodInvoker(sorter);
+        HuskyCoder<String> huskyCoder = (HuskyCoder<String>) privateMethodInvoker.invokePrivate("getHuskyCoder");
+        final int N = 100;
+        helper.init(N);
+        final String[] xs = helper.random(String.class, r -> r.nextLong() + "");
+        Coding coding = huskyCoder.huskyEncode(xs);
+        PureHuskySort.insertionSort(xs, coding.longs, 0, N);
+        assertEquals(0, helper.inversions(xs));
+    }
+
+    @Test
+    public void testInsertionSort() {
+        PureHuskySort<String> sorter = new PureHuskySort<>(HuskyCoderFactory.asciiCoder, false, false);
+        PrivateMethodInvoker privateMethodInvoker = new PrivateMethodInvoker(sorter);
+        HuskyCoder<String> huskyCoder = (HuskyCoder<String>) privateMethodInvoker.invokePrivate("getHuskyCoder");
+        final int N = 100;
+        helper.init(N);
+        final String[] xs = helper.random(String.class, r -> r.nextLong() + "");
+        Coding coding = huskyCoder.huskyEncode(xs);
+        sorter.insertionSort(xs, coding.longs, 0, N);
+        assertEquals(0, helper.inversions(xs));
+    }
+}
